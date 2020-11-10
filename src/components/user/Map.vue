385152827f0e0514a21789fba18519228bcd36cc
Logan Troyer <troyerlogan@gmail.com>
2020-11-10 11:14:53 -0500

    Testing axios on vercel

--- src/components/user/Map.vue
+++ src/components/user/Map.vue
@@ -20,6 +20,7 @@
 
 <script>
 import { mapState } from 'vuex';
+import axios from 'axios';
 
 export default {
   name: 'Map',
@@ -30,6 +31,14 @@ export default {
     ]),
   },
   mounted() {
+
+    axios.get(`https://gckm6smf0j.execute-api.us-east-1.amazonaws.com/user?userId=fc9ee2b7-aa3f-4148-9b88-477b27350ce5`)
+      .then(response => {
+        console.log(response)
+      }).catch(err => {
+        console.log(err);
+      })
+    
     this.$getLocation()
       .then(coordinates => {
         this.$store.dispatch('setCoordinates', coordinates);
